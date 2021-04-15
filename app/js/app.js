b1d7bcff40f8afe87979f70ae24d0b2640d749ff
CJEF <ef2200@mail.ru>
2021-04-15 15:48:22 +0600

    commit

--- app/js/app.js
+++ app/js/app.js
@@ -11,3 +11,40 @@ const swiper = new Swiper(".swiper-container", {
 });
 
 AOS.init();
+
+// TweenLite.to("#path", 1, {drawDVG:"50% 50%", delay: 3})
+// TweenLite.to("#path2", 1, {drawDVG:"50% 50%", delay: 3})
+// TweenLite.to("#path3", 1, {drawDVG:"50% 50%", delay: 3})
+// TweenLite.to("#svg", { rotation: 27, x: 100, duration: 1 });
+
+// new Vivus("svg", {
+//   duration: 5000,
+//   type: "oneByOne",
+// });
+
+// function fillPath(classname, color) {
+//   const paths = document.querySelectorAll(`#svg-castle .${classname}`);
+//   for (path of paths) {
+//     path.style.fill = `${color}`;
+//   }
+// }
+
+let aspro = document.querySelector("#aspro");
+let hoverBlock1 = document.querySelector("#hoverblock1");
+let hoverBlock2 = document.querySelector(".card-item__mockup--hover");
+let hoverBlock1Before = window.getComputedStyle(
+  document.querySelector("#hoverblock1"),
+  ":before"
+).transform;
+
+console.log(hoverBlock1Before);
+
+
+hoverBlock1.addEventListener("mouseenter", function (e) {
+  aspro.classList.add("scale")
+  hoverBlock1Before.value = "scale(1.2)";
+});
+
+hoverBlock1.addEventListener("mouseleave", function (e) {
+  aspro.classList.remove("scale");
+});
