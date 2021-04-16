231fa8dbe158c7a8d1a9df4b2c0e17f20d843f12
CJEF <ef2200@mail.ru>
2021-04-16 09:37:26 +0600

    fix colors and line hights

--- app/js/app.js
+++ app/js/app.js
@@ -32,8 +32,11 @@ AOS.init();
 let aspro = document.querySelector("#aspro");
 let prototype = document.querySelector("#prototype");
 let hoverBlock1 = document.querySelector("#hoverblock1");
-let hoverBlock2 = document.querySelector(".card-item__mockup--hover");
-
+let hoverBlock2 = document.querySelector("#hoverBlock2");
+let hoverBlock3 = document.querySelector("#hoverBlock3");
+let hoverBlock4 = document.querySelector("#hoverBlock4");
+let ecoton = document.querySelector(".card-item__ecoton");
+let iphone = document.querySelector(".process__prototype-iphone");
 
 hoverBlock1.addEventListener("mouseenter", function (e) {
   aspro.classList.add("scale")
@@ -57,5 +60,29 @@ hoverBlock2.addEventListener("mouseenter", function (e) {
 hoverBlock2.addEventListener("mouseleave", function (e) {
   prototype.classList.remove("scale");
   let wrap = prototype.closest(".card-item");
+  wrap.classList.remove("card-item--hover");
+});
+
+hoverBlock3.addEventListener("mouseenter", function (e) {
+  ecoton.classList.add("scale");
+  let wrap = ecoton.closest(".card-item");
+  wrap.classList.add("card-item--hover");
+});
+
+hoverBlock3.addEventListener("mouseleave", function (e) {
+  ecoton.classList.remove("scale");
+  let wrap = ecoton.closest(".card-item");
+  wrap.classList.remove("card-item--hover");
+});
+
+hoverBlock4.addEventListener("mouseenter", function (e) {
+  iphone.classList.add("scale");
+  let wrap = iphone.closest(".card-item");
+  wrap.classList.add("card-item--hover");
+});
+
+hoverBlock4.addEventListener("mouseleave", function (e) {
+  iphone.classList.remove("scale");
+  let wrap = iphone.closest(".card-item");
   wrap.classList.remove("card-item--hover");
 });
